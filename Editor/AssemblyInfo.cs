dc896eeb2180a72283679ab69b9277db9ec8f698
rawdreen <askarovabdulloh111@gmail.com>
2021-10-27 17:40:29 +0500

    fix: first commit

---
+++ Editor/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Runtime.CompilerServices;
+#if UNITY_EDITOR
+[assembly: InternalsVisibleTo("Unity.Perception.Editor")]
+#endif
+[assembly: InternalsVisibleTo("Unity.Perception.Editor.Tests")]
+[assembly: InternalsVisibleTo("Unity.Perception.Runtime.Tests")]
